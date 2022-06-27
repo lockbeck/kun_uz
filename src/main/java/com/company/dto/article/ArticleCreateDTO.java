504c466e387f2634e3201cba45c7fe8456933acb
t.lochin.98 <t.lochin.98@gmail.com>
2022-06-28 00:05:24 +0500

    Initial commit

--- src/main/java/com/company/dto/article/ArticleCreateDTO.java
+++ src/main/java/com/company/dto/article/ArticleCreateDTO.java
@@ -14,21 +14,24 @@ import java.util.List;
 @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ArticleCreateDTO {
-    @NotNull(message = "title required")
+    @NotNull(message = "Title required MAZGI")
     @Size(min = 10, max = 255,message = "title must be between 10 and 255 characters")
     private String title;
 //    @NotNull(message = "description qani mazgi")
-    @NotBlank(message = "description qani mazgi")
+    @NotBlank(message = "Description required MAZGI")
     private String description;
-    @NotNull(message = "Content qani mazgi")
+    @NotNull(message = "Content required MAZGI")
     private String content;
 
     private String imageId;
-    @PositiveOrZero(message = "positive must be entered MAZGI")
+    @PositiveOrZero(message = "positive number must be entered as region id MAZGI")
     private Integer regionId;
+    @PositiveOrZero(message = "positive number must be entered as category id MAZGI")
     private Integer categoryId;
 
+    @NotNull(message = "typeList required MAZGI")
     private List<Integer> typesList;
+    @NotNull(message = "tagList required MAZGI")
     private List<String> tagList;
 
 }
